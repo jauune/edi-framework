97e20bf0c1602af79cd8e28170cfb08b4c75a917
Jonathan Van den Broeck <42971300+jauune@users.noreply.github.com>
2025-01-03 14:52:30 +0100

    get filenames correction

--- edi_storage_oca/models/edi_backend.py
+++ edi_storage_oca/models/edi_backend.py
@@ -161,9 +161,8 @@ class EDIBackend(models.Model):
         if exchange_type.exchange_file_ext:
             bits.append(r"\." + exchange_type.exchange_file_ext)
         pattern = "".join(bits)
-        full_paths = utils.find_files(self.storage_id, pattern, full_input_dir_pending)
-        pending_path_len = len(full_input_dir_pending)
-        return [p[pending_path_len:].strip("/") for p in full_paths]
+        filenames = [x for x in utils.find_files(self.storage_id, pattern, full_input_dir_pending) if x.strip("/")]
+        return filenames
 
     def _storage_new_exchange_record_vals(self, file_name):
         return {
